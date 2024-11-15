f9187a94e81b7630c4827269487fe3d49422ab61
saghar.rezaei <saghar.rezaei@tum.de>
2024-11-15 13:34:00 -0300

    first query

---
+++ Consulta1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from pymongo import MongoClient
+import pandas as pd
+
+client = MongoClient("mongodb://localhost:27017/")  
+db = client['Negocio']
+collection = db['Clientes']
+
+pipeline = [
+    {
+        "$lookup": {
+            "from": "Telefonos",        
+            "localField": "nro_cliente",
+            "foreignField": "nro_cliente", 
+            "as": "telefonos"             
+        }
+    }
+]
+
+result = list(db.Clientes.aggregate(pipeline))
+df = pd.DataFrame(result)
+
+df['telefonos'] = df['telefonos'].apply(lambda x: [f"{item['codigo_area']}-{item['nro_telefono']}" for item in x] if isinstance(x, list) else [])
+
+df = df[['nombre', 'apellido', 'telefonos']]
