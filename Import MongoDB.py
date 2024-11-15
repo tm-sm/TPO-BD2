b08f37fafb7f001b12ab225bf7380db8d9b5c8c8
saghar.rezaei <saghar.rezaei@tum.de>
2024-11-15 13:15:36 -0300

    remove comment

--- Import MongoDB.py
+++ Import MongoDB.py
@@ -7,10 +7,10 @@ client = MongoClient('mongodb://localhost:27017/')
 
 # clientes
 db = client['Negocio']
-#collection = db['Clientes']
-#data = pd.read_csv('Data/e01_cliente.csv', encoding = 'ANSI', delimiter=';')
-#records = data.to_dict(orient='records')
-#collection.insert_many(records)
+collection = db['Clientes']
+data = pd.read_csv('Data/e01_cliente.csv', encoding = 'ANSI', delimiter=';')
+records = data.to_dict(orient='records')
+collection.insert_many(records)
 
 #telefonos
 collection = db['Telefonos']
