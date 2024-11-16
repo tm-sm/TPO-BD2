89da46c876083a88b1e10c5d5c54dfb2646c6ecb
NCasella <nicolascasella02@gmail.com>
2024-11-16 13:51:14 -0300

    implemented consulta4

---
+++ Consulta4.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+from neo4j import GraphDatabase
+
+
+client = MongoClient("mongodb://localhost:27017/")  
+db = client['Negocio']
+collection = db['Clientes']
+
+driver=GraphDatabase.driver(uri="bolt://localhost:7687")   
+session=driver.session()
+
+clients= list(db.Clientes.find())
+
+result=[]
+for client in clients:
+    query="""MATCH (f:Factura {nro_cliente:$nro_cliente})
+    return f
+    """
+    neo_query_result=session.run(query,nro_cliente=client['nro_cliente'])
+    if neo_query_result:
+        result.append(client)
+for client in result:
+    print(client)
